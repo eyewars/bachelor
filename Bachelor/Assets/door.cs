4af0b6f1ca5b38434e0774144e81a21909c66142
Even Hvidsten <evenkh@hotmail.com>
2024-05-01 14:46:25 +0200

    finished small door

--- Bachelor/Assets/door.cs
+++ Bachelor/Assets/door.cs
@@ -1,21 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class door : MonoBehaviour, interactable{
     private Transform[] doors = new Transform[2];
-    
+    private Transform[] wheels = new Transform[2];
+
     public bool isOpen = false;
     private bool isUnlocked = false;
     public int keyId;
-        
+
+    public bool isBig;
+
     public float doorSpeed;
     public float openRange;
 
     public LayerMask playerMask;
     public LayerMask wallMask;
-    
+
     public void interact() {
         if (!isUnlocked) {
             if (playerManager.instance.keyIds.Contains(keyId)) {
@@ -23,15 +27,24 @@ public class door : MonoBehaviour, interactable{
             }
         }
     }
-    
+
     public void unlockDoor() {
         isUnlocked = true;
     }
 
     void Start() {
         // PASS PÅ NÅR DERE BUILDER AT DISSE INDEXENE BLIR RIKTIGE!!!!!
-        doors[0] = transform.GetChild(0).GetChild(0);
-        doors[1] = transform.GetChild(0).GetChild(1);
+        if (isBig) {
+            doors[0] = transform.GetChild(0).GetChild(0);
+            doors[1] = transform.GetChild(0).GetChild(1);
+        } else {
+            doors[0] = transform.GetChild(2).GetChild(0);
+            doors[1] = transform.GetChild(2).GetChild(1);
+
+            wheels[0] = doors[0].GetChild(0);
+            wheels[1] = doors[1].GetChild(0);
+        }
+
 
         StartCoroutine(doorTimer());
 
@@ -41,24 +54,78 @@ public class door : MonoBehaviour, interactable{
             isUnlocked = false;
         }
     }
-    
+
     void Update() {
-        if (isOpen) {
-            if (doors[0].localPosition.x < 0.017f) {
-                doors[0].localPosition = new Vector3(doors[0].localPosition.x + (0.001f * doorSpeed * Time.deltaTime), 0f, 0f);
-                doors[1].localPosition = new Vector3(doors[1].localPosition.x - (0.001f * doorSpeed * Time.deltaTime), 0f, 0f);
-            } else if (doors[0].localPosition.x < 0.017f) {
-                doors[0].localPosition = new Vector3(0.017f, 0f, 0f);
-                doors[1].localPosition = new Vector3(-0.017f, 0f, 0f);
+        if (isBig) {
+            if (isOpen) {
+                if (doors[0].localPosition.x < 0.017f) {
+                    doors[0].localPosition = new Vector3(doors[0].localPosition.x + (0.001f * doorSpeed * Time.deltaTime), 0f, 0f);
+                    doors[1].localPosition = new Vector3(doors[1].localPosition.x - (0.001f * doorSpeed * Time.deltaTime), 0f, 0f);
+                } else if (doors[0].localPosition.x > 0.017f) {
+                    doors[0].localPosition = new Vector3(0.017f, 0f, 0f);
+                    doors[1].localPosition = new Vector3(-0.017f, 0f, 0f);
+                }
+            } else {
+                if (doors[0].localPosition.x > 0f) {
+                    doors[0].localPosition = new Vector3(doors[0].localPosition.x - (0.001f * doorSpeed * Time.deltaTime), 0f, 0f);
+                    doors[1].localPosition = new Vector3(doors[1].localPosition.x + (0.001f * doorSpeed * Time.deltaTime), 0f, 0f);
+                } else if (doors[0].localPosition.x < 0f) {
+                    doors[0].localPosition = new Vector3(0f, 0f, 0f);
+                    doors[1].localPosition = new Vector3(0f, 0f, 0f);
+                }
             }
         } else {
-            if (doors[0].localPosition.x > 0f) {
-                doors[0].localPosition = new Vector3(doors[0].localPosition.x - (0.001f * doorSpeed * Time.deltaTime), 0f, 0f);
-                doors[1].localPosition = new Vector3(doors[1].localPosition.x + (0.001f * doorSpeed * Time.deltaTime), 0f, 0f);
-            } else if (doors[0].localPosition.x < 0f) {
-                doors[0].localPosition = new Vector3(0f, 0f, 0f);
-                doors[1].localPosition = new Vector3(0f, 0f, 0f);
-            }
+            Debug.Log(doors[1].localPosition);
+
+
+            if (isOpen) {
+                // LOKKA: -0.2376854
+                // ÅPEN: 1.471
+                if (doors[1].localPosition.x < 1.471f) {
+                    Debug.Log(" TEHT HAE HUTIOIHUAETHIUAEHIUOTHUIPAET");
+                        doors[1].localPosition = new Vector3(doors[1].localPosition.x + (0.2f * doorSpeed * Time.deltaTime), -0.08292082f, -0.7379031f);
+                    } else if (doors[1].localPosition.x >= 1.471f) {
+                        doors[1].localPosition = new Vector3(1.471f, -0.08292082f, -0.7379031f);
+
+                        if (wheels[0].localEulerAngles.y < 170f) {
+                            Vector3 currentAngles = wheels[0].localEulerAngles;
+                            currentAngles.y += 50f * doorSpeed * Time.deltaTime;
+                            wheels[0].localEulerAngles = currentAngles;
+                            wheels[1].localEulerAngles = currentAngles;
+                        } else if (wheels[0].localEulerAngles.y >= 170f) {
+                            wheels[0].localEulerAngles = new Vector3(0f, 171f, 0f);
+                            wheels[1].localEulerAngles = new Vector3(0f, 171f, 0f);
+
+                            if (doors[0].localPosition.x < 1.471f) {
+                                doors[0].localPosition = new Vector3(doors[0].localPosition.x + (0.2f * doorSpeed * Time.deltaTime), 0.1042567f, -0.7379031f);
+                            } else if (doors[0].localPosition.x > 1.471f) {
+                                doors[0].localPosition = new Vector3(1.471f, 0.1042567f, -0.7379031f);
+                            }
+                        }
+                    }
+                } else {
+                if (doors[1].localPosition.x > -0.2376854f) {
+                        doors[1].localPosition = new Vector3(doors[1].localPosition.x - (0.2f * doorSpeed * Time.deltaTime), -0.08292082f, -0.7379031f);
+                    } else if (doors[1].localPosition.x <= -0.2376854f) {
+                        doors[1].localPosition = new Vector3(-0.2376854f, -0.08292082f, -0.7379031f);
+
+                        if (wheels[0].localEulerAngles.y > 10f) {
+                            Vector3 currentAngles = wheels[0].localEulerAngles;
+                            currentAngles.y -= 40f * doorSpeed * Time.deltaTime;
+                            wheels[0].localEulerAngles = currentAngles;
+                            wheels[1].localEulerAngles = currentAngles;
+                        } else if (wheels[0].localEulerAngles.y <= 10f) {
+                            wheels[0].localEulerAngles = new Vector3(0f, 9f, 0f);
+                            wheels[1].localEulerAngles = new Vector3(0f, 9f, 0f);
+
+                            if (doors[0].localPosition.x > -0.2376854f) {
+                                doors[0].localPosition = new Vector3(doors[0].localPosition.x - (0.2f * doorSpeed * Time.deltaTime), 0.1042567f, -0.7379031f);
+                            } else if (doors[0].localPosition.x < -0.2376854f) {
+                                doors[0].localPosition = new Vector3(-0.2376854f, 0.1042567f, -0.7379031f);
+                            }
+                        }
+                    }
+                }
         }
     }
 
@@ -86,7 +153,7 @@ public class door : MonoBehaviour, interactable{
             if (isUnlocked) {
                 checkPlayerProximity();
             }
-            
+
             yield return new WaitForSeconds(0.2f);
         }
     }
