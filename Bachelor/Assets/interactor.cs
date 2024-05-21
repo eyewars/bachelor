eb8a1a3dd81dbabc3e2beb2e746063090cc2e040
Even Hvidsten <evenkh@hotmail.com>
2024-05-21 12:03:50 +0200

    can interact with terminal

--- Bachelor/Assets/interactor.cs
+++ Bachelor/Assets/interactor.cs
@@ -11,14 +11,25 @@ public class interactor : MonoBehaviour{
 
     public Camera myCamera;
 
+    private bool isTyping;
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.F)) {
             Ray r = new Ray(myCamera.transform.position, myCamera.transform.forward);
             if (Physics.Raycast(r, out RaycastHit hitInfo, interactRange)) {
                 if (hitInfo.collider.gameObject.TryGetComponent(out interactable interactObj)) {
                     interactObj.interact();
+
+                    if (hitInfo.collider.gameObject.tag == "terminal") {
+                        isTyping = true;
+                        Debug.Log(isTyping);
+                    }
                 }
+            } else {
+                isTyping = false;
             }
+        } else if (Input.GetKeyUp(KeyCode.F)) {
+            isTyping = false;
         }
     }
 }
---
+++ Bachelor/Assets/terminal.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terminal : MonoBehaviour, interactable{
+    void Start() { }
+    
+    void Update() { }
+
+    public void interact() {
+        Debug.Log("NÅ TRYTTJA ME");
+    }
+}
